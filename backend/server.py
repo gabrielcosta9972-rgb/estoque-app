efcb848daad5f7f64a5035e79e0a81475e14bd2c
Gabriel <gabrielcosta9.9@hotmail.com>
2026-05-18 01:49:53 -0300

    Atualização

--- backend/server.py
+++ backend/server.py
@@ -66,7 +66,7 @@ STORE_ALIASES = {
     "bar": "Bar",
 }
 
-CATEGORIES = ["Mercearia", "Resfriados", "Limpeza", "Congelados,"
+CATEGORIES = ["Mercearia", "Resfriados", "Limpeza", "Congelados"
 "","Embalagens", "Hortifruti", "Bebidas", "Doces", "Outros"]
 
 
